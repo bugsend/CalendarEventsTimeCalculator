3f7d32c12c8b8a86a86a8c13fd68501fc5cad3b8
Sergey Zaytsev <bugsend@icloud.com>
2017-09-04 22:06:47 +0300

    Initial Commit

---
+++ CalendarEventDurationCounter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CalendarEventDurationCounter
+//
+//  Created by Sergey Zaytsev on 04/09/2017.
+//  Copyright © 2017 Sergey Zaytsev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
